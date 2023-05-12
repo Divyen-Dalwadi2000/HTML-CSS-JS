e8f5f5b475c3e2786cb598e59f4b424b12393453
Mrdivyendalwadi <114602971+Divyen-Dalwadi2000@users.noreply.github.com>
2023-05-12 18:16:19 +0530

    Create one.js

---
+++ THAPA/one.js
@@ -0,0 +1,13 @@
+console.log(10 + " 20 ");           // 10 20
+console.log(9 - " 5 ");             // 4            // BUG => bcz 9 is number and 5 is string
+console.log("Java " + "Script");    // Java Script
+console.log(" " + " ");             // 
+console.log(" " + 0 );              //  0
+console.log("Divyen" - "Dalwadi");  // NaN          (Not a Number)
+
+// 1 true
+// 0 false
+console.log(true + true);           // 2            (1+1)
+console.log(true + false);          // 1            (1+0)
+console.log(false + true);          // 1            (0+1)
+console.log(false - true);          // -1           (0-1)
